8014c75345aff71c5dc059f10c603dfc2ce03585
Akira Yoshiyama <akirayoshiyama@gmail.com>
2013-08-14 01:08:12 +0900

    Removed an unused template filter.

--- logcas/template_filters.py
+++ logcas/template_filters.py
@@ -7,8 +7,3 @@ from logcas.bootstrap import *
 @app.template_filter('localtime')
 def _localtime_filter(dtime):
     return dtime.replace(microsecond=0, tzinfo=None)
-
-
-@app.template_filter('unixtime')
-def _unixtime_filter(dtime):
-    return int(time.mktime(dtime.timetuple()))
